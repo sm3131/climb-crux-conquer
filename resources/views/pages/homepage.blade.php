5cc53fc6abc3b862f8f1f07e120571ae80f81000
Sammi Moore <sm2683@nau.edu>
2022-09-28 16:38:55 -0700

    created initial layout and added text to homepage; work in progress; added links to header tabs

--- resources/views/pages/homepage.blade.php
+++ resources/views/pages/homepage.blade.php
@@ -1,8 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-<div>
-    <h1 class="text-3xl bg-slate-blue text-orange text-center">This is the HomePage!</h1>
-</div>
-
+    <div class="bg-slate-blue flex flex-col gap-11 items-center">
+        <h2 class="text-orange text-center text-4xl w-1/3 flex-auto pt-4">Welcome to Climb, Crux, Conquer where you can plan
+            your next climbing conquest, discuss route beta with other climbers, and keep track of your current and
+            future projects!</h2>
+        <h3 class="text-center text-light-gray text-lg w-5/6 pb-4">Browse the current climbs on the View Climbs page, or sign up to create an
+            account and take advantage of all the additional features, including, tracking your climbing progress and
+            discussing climbs with fellow climbers</h3>
+    </div>
 @endsection
